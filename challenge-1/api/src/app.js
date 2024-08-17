00a87ce6469512c753124f892fc931288470eef3
LiamOtegui <liamotegui@gmail.com>
2024-08-17 00:03:37 -0300

    errorMiddleware added

--- challenge-1/api/src/app.js
+++ challenge-1/api/src/app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 // const router = require('router')
-// const errorMiddleware()
+const errorMiddleware = require('../middlewares/errorMiddleware')
+
+app.get('/', () => {
+    throw new Error('Fake error')
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 // app.use(router)
-// app.use(errorMiddleware)
+app.use(errorMiddleware)
 
 module.exports = app
