bbc24b2d835ccf90e42f6931fac49f703c2d4a2d
LiamOtegui <liamotegui@gmail.com>
2024-08-18 01:59:15 -0300

    Tailwind and react-router-dom configuration

--- challenge-2/client/src/App.jsx
+++ challenge-2/client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
-    <>
-      hola
-    </>
+    <div>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </div>
   )
 }
 
